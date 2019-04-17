59bb97bfb29121d2ec4ca49bf3d2408c8f46187e
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-04-17 14:42:54 -0700

    added code to continue buidling syn generator

--- unit_1/app.js
+++ unit_1/app.js
@@ -8,31 +8,57 @@ const addWord = (word) => {
   const $flexContainer = $( '.flex-container' );
 
   //create a card with the word
-    const $card = $( '<div>' ).addClass( 'card' ).text( word);
+    const $card = $( '<div>' ).addClass( 'card' ).text(word);
 
   //add an event listner/handler that toggles the class selected for the card that is clicked
-    $card.on( 'click', ( e ) => {
-      $( e.target ).toggleClass( 'selected' );
-    });
+
+  // add some functionality later...
+    // $card.on( 'click', ( e ) => {
+    //   $( e.target ).toggleClass( 'selected' );
+    // });
 
     //append the card to the container
     $flexContainer.append( $card );
   }
-});
 
-let randomWord = "blue"
-//random word array to randomize the words 
+
+let randomWord = "cat"
+//random word array to randomize the words
       $.ajax({
     url:'http://words.bighugelabs.com/api/2/7ee9eb246b6c90b808629855dae0af74/'+randomWord+'/json'
 }).then(
     (data)=>{
+      const parsedWords = JSON.parse(data).noun.syn;
+      //created the variable for retrieving the parsed data
+      //define a max variable, the stopping point:
+      let maxLength = 0
+      if(parsedWords.length < 8){
+      maxLength = parsedWords.length
+    } else { maxLength = 8
+      };
+      //.length array method for iterating through an array
+      //if length of parsedWords is less than the number I established,
+      //the length is always one more than
+      for(i=0; i < maxLength; i++) {
+      console.log(parsedWords[i]);
+      addWord (parsedWords[i]);
+      //goal: log each word as we are going through the loop
+      //with the i referring to the words in the array
+      //take addWord and call it within the for loop
+    };
+    //if statement before the for loop, if the length is less than I'd like, make it so the stopping point is whatever I choose. Endpoint at data.length if less than the number I establish.
+      //creating for loop to iterate over the array
         console.log(JSON.parse(data).noun.syn);
         // loop over this array of words and pass in the current word into the previous function and it should then take the place of letter---I should be able to see all the words on the page
+
     },
     ()=>{
         console.log('bad request');
-    }
+  }
+
+
 );
+});
 // click event, search for the word according to when they click the get synoymn button
 
 
